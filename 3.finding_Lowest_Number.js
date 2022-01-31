db117d2471bc3a74683fe9670361fac8b0154cb0
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-01-31 23:17:17 +0600

    Adding third problem

---
+++ 3.finding_Lowest_Number.js
@@ -0,0 +1,11 @@
+let a = 2;
+let b = 5;
+let c = -6;
+
+if (a < b && a < c) {
+  console.log("The Lowest number is: a= " + a);
+} else if (b < a && b < c) {
+  console.log("The Lowest number is: b= " + b);
+} else {
+  console.log("The Lowest number is: c= " + c);
+}
