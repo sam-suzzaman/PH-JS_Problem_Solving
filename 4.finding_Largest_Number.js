d861dd973ef245e54d8e194f5d0ffc8abae3984b
samsuzzaman1998 <dksamsuzzaman@gmail.com>
2022-02-01 09:37:02 +0600

    adding fouth problem

---
+++ 4.finding_Largest_Number.js
@@ -0,0 +1,11 @@
+let a = -10;
+let b = -555;
+let c = -33;
+
+if (a > b && a > c) {
+  console.log("a= " + a + " is the Largest number.");
+} else if (b > a && b > c) {
+  console.log("b= " + b + " is the Largest number.");
+} else {
+  console.log("c= " + c + " is the Largest number.");
+}
